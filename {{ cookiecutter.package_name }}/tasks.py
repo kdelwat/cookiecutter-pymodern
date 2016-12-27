d843880ea4359f059eee8c1ecc4c2e559fc0fe41
Cadel Watson <cadel@cadelwatson.com>
2016-12-27 21:26:27 +1100

    Add PyPI registration and upload with twine

--- {{ cookiecutter.package_name }}/tasks.py
+++ {{ cookiecutter.package_name }}/tasks.py
@@ -12,3 +12,15 @@ def clean(context):
 def build(context, clean=False):
     context.run('python setup.py sdist')
     context.run('python setup.py bdist_wheel')
+
+
+@task(pre=[build])
+def register(context):
+    name = '{{ cookiecutter.package_name }}-{{ cookiecutter.version }}'
+    context.run('twine register dist/{0}.tar.gz'.format(name))
+    context.run('twine register dist/{0}-py3-none-any.whl'.format(name))
+
+
+@task
+def upload(context):
+    context.run('twine upload dist/*')
