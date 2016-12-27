aeb5b5114304d5f98f3cf28926befa69d9d4d39a
Cadel Watson <cadel@cadelwatson.com>
2016-12-27 21:28:06 +1100

    Clean before building package

--- {{ cookiecutter.package_name }}/tasks.py
+++ {{ cookiecutter.package_name }}/tasks.py
@@ -8,8 +8,8 @@ def clean(context):
     context.run('rm -rf *.egg-info/')
 
 
-@task
-def build(context, clean=False):
+@task(pre=[clean])
+def build(context):
     context.run('python setup.py sdist')
     context.run('python setup.py bdist_wheel')
 
